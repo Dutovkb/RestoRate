1eb891149f1213c37dfbe4da68b97d6e9d3d4438
Kirill Dutov <dutovapple@yandex.ru>
2021-02-05 20:40:16 +0300

    Rename

---
+++ Restorate/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Planner
+//
+//  Created by Кирилл Дутов on 05.02.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
