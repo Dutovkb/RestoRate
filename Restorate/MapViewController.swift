0192997703e8d7f1a195ebdf66b18ee2d5989296
Kirill Dutov <dutovapple@yandex.ru>
2021-02-09 20:12:35 +0300

    Added a new map screen and implemented the transition and exit logic

---
+++ Restorate/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Restorate
+//
+//  Created by Кирилл Дутов on 09.02.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
